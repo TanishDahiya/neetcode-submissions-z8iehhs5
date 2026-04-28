1013e96b4c1800d0aaa0fd88d2fb592443486548
Tanish Dahiya <48643197+TanishDahiya@users.noreply.github.com>
2026-04-28 11:57:30 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int buyPrice = prices[0];
+        int maxProfit = 0;
+
+        for(int i =0;i<n;i++){
+            if(buyPrice > prices[i]){
+                buyPrice = prices[i];
+            }else{
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit,profit);
+            }
+        }
+
+        return maxProfit;
+    }
+}
