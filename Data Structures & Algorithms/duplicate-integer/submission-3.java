751386b5ce409470204261762f1dacf1ccd6a605
Tanish Dahiya <48643197+TanishDahiya@users.noreply.github.com>
2026-05-11 11:32:15 +0530

    Rename method and optimize duplicate check
    Refactor method to check for duplicates using a HashMap instead of a HashSet.

--- Data Structures & Algorithms/duplicate-integer/submission-3.java
+++ Data Structures & Algorithms/duplicate-integer/submission-3.java
@@ -1,16 +1,13 @@
 class Solution {
-    public boolean hasDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
 
-        for(int num: nums){
-            if(set.contains(num)) return true;
-            set.add(num);
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0)+1);
+
+            if(map.get(num) > 1) return true;
         }
 
         return false;
     }
 }
-
-
-// Previous approach O(n log n) + O(n) ~= O(n log n)
-// Current TC :O(n) space: O(n)
